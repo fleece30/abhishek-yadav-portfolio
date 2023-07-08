10afaba3667d43ff254b2ddd281325471f25331a
fleece30 <abhiyad25@gmail.com>
2023-07-08 19:51:13 +0530

    server side code debugged

--- pages/blog/[post].tsx
+++ pages/blog/[post].tsx
@@ -36,31 +36,12 @@ const Post: React.FC<PostProps> = ({ data }) => {
 };
 
 export const getServerSideProps = async (context: any) => {
-  // const { data } = await apollo.query({
-  //   query: getPostByIdQuery,
-  //   variables: {
-  //     postId: context.params.post,
-  //   },
-  // });
-  // console.log(data);
-  const data = {
-    postbyid: {
-      __typename: "Post",
-      id: "64a822d119e5695955906c43",
-      title: "The ghosts of childhood",
-      content:
-        "The ghosts of many childhoods,\n" +
-        "Roam this house of yore.\n" +
-        "Reliving tales of a time long gone,\n" +
-        "Of gully cricket and of stapu and more.\n" +
-        "\n" +
-        "To dancing in the rain and,\n" +
-        "The pakodas we used to adore,\n" +
-        "To a new book and home-grown mangoes,\n" +
-        "Take me back once more.",
-      createdAt: "2023-07-07",
+  const { data } = await apollo.query({
+    query: getPostByIdQuery,
+    variables: {
+      postId: context.params.post,
     },
-  };
+  });
   return {
     props: {
       data: data.postbyid,

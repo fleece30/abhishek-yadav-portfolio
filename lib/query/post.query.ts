bc4705769b2d72314f1eba8332f24a699f4723a7
fleece30 <abhiyad25@gmail.com>
2023-07-08 19:47:04 +0530

    updated query

--- lib/query/post.query.ts
+++ lib/query/post.query.ts
@@ -1,5 +1,6 @@
 import { builder } from "../builder";
 import { prisma } from "../prisma";
+import { PrismaPromise } from ".prisma/client";
 
 builder.queryField("posts", (t) =>
   t.prismaField({
@@ -16,8 +17,9 @@ builder.queryField("postbyid", (t) =>
     args: {
       postId: t.arg.string({ required: true }),
     },
-    resolve: async (query, parents, args) => {
-      return prisma.post.findUnique({ ...query, where: { id: args.postId } });
+    // @ts-ignore
+    resolve: async (query, _parents, _args, _info) => {
+      return prisma.post.findUnique({ ...query, where: { id: _args.postId } });
     },
   })
 );
